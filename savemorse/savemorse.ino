2fa141c337d54d5696d4282b8712398e170e6c5e
Jonathan <jonathan.niklasson@gmail.com>
2016-03-07 21:57:20 +0100

    pointers eh?

--- savemorse/savemorse.ino
+++ savemorse/savemorse.ino
@@ -4,48 +4,48 @@
 #define DASH    1
 #define M_END   255
 
-int morse_codes[][27] = {
-    {DOT, DASH, M_END},             // 'A'
-    {DASH, DOT, DOT, DOT, M_END},   // 'B'
-    {DASH, DOT, DASH, DOT, M_END},  // 'C'
-    {DASH, DOT, DOT, M_END},        // 'D'
-    {DOT, M_END},                   // 'E'
-    {DOT, DOT, DASH, DOT, M_END},   // 'F'
-    {DASH, DASH, DOT, M_END},       // 'G'
-    {DOT, DOT, DOT, DOT, M_END},    // 'H'
-    {DOT, DOT, M_END},              // 'I'
-    {DOT, DASH, DASH, DASH, M_END}, // 'J'
-    {DASH, DOT, DASH, M_END},       // 'K'
-    {DOT, DASH, DOT, DOT, M_END},   // 'L'
-    {DASH, DASH, M_END},            // 'M'
-    {DOT, DASH, M_END},             // 'N'
-    {DASH, DASH, DASH, M_END},      // 'O'
-    {DOT, DASH, DASH, DOT, M_END},  // 'P'
-    {DASH, DASH, DOT, DASH, M_END}, // 'Q'
-    {DOT, DASH, DOT, M_END},        // 'R'
-    {DOT, DOT, DOT, M_END},         // 'S'
-    {DASH, M_END},                  // 'T'
-    {DOT, DOT, DASH, M_END},        // 'U'
-    {DOT, DOT, DOT, DASH, M_END},   // 'V'
-    {DOT, DASH, DASH, M_END},       // 'W'
-    {DASH, DOT, DOT, DASH, M_END},  // 'X'
-    {DASH, DOT, DASH, DASH, M_END}, // 'Y'
-    {DASH, DASH, DOT, DOT, M_END},  // 'Z'
-    {DASH, M_END}                   // 'alt' (used for space)
+uint8_t morse_codes[][27] = {
+    {DOT, DASH, M_END,  0},             // 'A'
+    {DASH, DOT, DOT,  DOT},             // 'B'
+    {DASH, DOT, DASH,  DOT},            // 'C'
+    {DASH, DOT, DOT, M_END},            // 'D'
+    {DOT, M_END, 0, 0},                 // 'E'
+    {DOT, DOT, DASH, DOT},              // 'F'
+    {DASH, DASH, DOT, M_END},           // 'G'
+    {DOT, DOT, DOT, DOT},               // 'H'
+    {DOT, DOT, M_END, 0},               // 'I'
+    {DOT, DASH, DASH, DASH},            // 'J'
+    {DASH, DOT, DASH, M_END},           // 'K'
+    {DOT, DASH, DOT, DOT},              // 'L'
+    {DASH, DASH, M_END, 0},             // 'M'
+    {DOT, DASH, M_END, 0},              // 'N'
+    {DASH, DASH, DASH, M_END},          // 'O'
+    {DOT, DASH, DASH,  DOT},            // 'P'
+    {DASH, DASH, DOT, DASH},            // 'Q'
+    {DOT, DASH, DOT, M_END},            // 'R'
+    {DOT, DOT, DOT,  M_END},            // 'S'
+    {DASH, M_END, 0, 0},                // 'T'
+    {DOT, DOT, DASH, M_END},            // 'U'
+    {DOT, DOT, DOT,   DASH},            // 'V'
+    {DOT, DASH, DASH, M_END},           // 'W'
+    {DASH, DOT, DOT,   DASH},           // 'X'
+    {DASH, DOT, DASH,  DASH},           // 'Y'
+    {DASH, DASH, DOT,   DOT},           // 'Z'
+    {DASH, M_END, 0, 0}                       // 'alt' (used for space)
 };
 
 void EEPROM_SAVE ( void ) {
   // consume array and save it in EEPROM
-  // Data is stored in chunks of 4 bites
-  // cause that is easier
-   int addr = 0;
-   int *get_seq;
+  // stops saving at M_END 
+   uint8_t addr = 0;
+   uint8_t *get_seq;
    for (int i=0; i < 27; i+1) {
     //0 1 2 3 4 5...
       get_seq = morse_codes[i];
       for (int k=0; k < k+3; k++){
         //0 1 2 3
         if ( get_seq[i] == M_END) {
+          EEPROM.write(addr, get_seq[i]);
           addr++;
           break;  
         }
